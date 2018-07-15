4091a63e01830acf4827decbfe7f712c312d321e
Nikita Zaytsev <nikitos945max@gmail.com>
2018-07-16 02:43:01 +0300

    сitizen

--- contracts/citizen.sol
+++ contracts/citizen.sol
@@ -1,6 +1,30 @@
 pragma solidity ^0.4.22;
 
 contract Citizen {
+  bool private isDead;
+  uint private votes;
+
+  constructor() {
+    isDead = false;
+    votes = 0;
+  }
+
+
+  function addVote() {
+    votes += 1;
+  }
+
+  function getVotes() returns (uint) {
+    return votes;
+  }
+
+  function Kill() {
+    isDead = true;
+  }
+
+  function Vote(address addr) {
+
+  }
 
 
 }
