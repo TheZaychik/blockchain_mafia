b5ac92349e7389eedc2f79badff609dd63d53834
Nikita Zaytsev <nikitos945max@gmail.com>
2018-07-16 03:08:16 +0300

    чутка gamesys

---
+++ contracts/gamesys.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.4.22;
+
+import "./citizen.sol";
+import "./doctor.sol";
+import "./donmafia.sol";
+import "./kommisar.sol";
+import "./mafia.sol";
+import "./mabiac.sol";
+
+contract GameSys {
+    address[] players;
+
+    function getMaxVotes() returns (address) {
+
+    }
+
+    
+}
