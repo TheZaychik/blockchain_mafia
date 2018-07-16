64d8d2546339088a814cf6724057a0857848688b
Nikita Zaytsev <nikitos945max@gmail.com>
2018-07-16 11:49:43 +0300

    cit

--- contracts/citizen.sol
+++ contracts/citizen.sol
@@ -5,12 +5,12 @@ import "./doctor.sol";
 import "./donmafia.sol";
 import "./kommisar.sol";
 import "./mafia.sol";
-import "./mainac.sol";
+import "./mabiac.sol";
 
 contract Citizen {
   bool public isDead;
   uint public votes;
-  bool public isSleeping;
+  bool private isSleeping;
 
   uint public constant typeContract = 0; // citizen = 0, doctor = 1, donmafia = 2, kommisar = 3, mafia = 4, mania
   constructor() {
@@ -37,23 +37,25 @@ contract Citizen {
 
   function vote(address addr, uint typeContr) {
     if (typeContr == 0){
-        Citizen(addr).addVote();
+       Citizen(addr).addVote();
     }
     else if (typeContr == 1){
-        Doctor(addr).addVote();//remake
+       Doctor(addr).addVote();//remake
     }
     else if (typeContr == 2){
-        DonMafia(addr).addVote();//remake
+       DonMafia(addr).addVote();//remake
+
     }
     else if (typeContr == 3){
-        Kommisar(addr).addVote();
+       Kommisar(addr).addVote();
+
     }
     else if (typeContr == 4){
-        Mafia(addr).addVote();//remake
+       Mafia(addr).addVote();//remake
     }
     else if (typeContr == 5){
-        Maniac(addr).addVote();//remake
+       Maniac(addr).addVote();//remake
     }
-  }
 
+  }
 }
