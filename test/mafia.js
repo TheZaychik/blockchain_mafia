58f66c8a079b6a5299a96b6b0ea97de0bcabd1ac
Nikita Zaytsev <nikitos945max@gmail.com>
2018-07-16 14:09:55 +0300

    tests v1

--- test/mafia.js
+++ test/mafia.js
@@ -8,10 +8,30 @@ var g = artifacts.require("./maniac.sol");
 
 contract('mafia', function(accounts) {
   var citizen1, citizen2, citizen3, mafia, kommisar, doctor;
-  it("Citizens is created", async function() {
+  it("Players are created", async function() {
     citizen1 = await a.new();
     citizen2 = await a.new();
     citizen3 = await a.new();
-    assert.equal(citizen1.isDead, false);
+    mafia = await f.new();
+    kommisar = await e.new();
+    maniac = await g.new();
+    doctor = await b.new();
+    assert.equal(await citizen1.isDead(), false);
+    assert.equal(await citizen2.isDead(), false);
+    assert.equal(await citizen3.isDead(), false);
+    assert.equal(await mafia.isDead(), false);
+    assert.equal(await kommisar.isDead(), false);
+    assert.equal(await doctor.isDead(), false);
+  });
+  it("Mafia, kommisar and maniac can kill someone", async function() {
+    mafia.kill(await citizen1.address, await citizen1.typeContract());
+    maniac.kill(await citizen2.address, await citizen2.typeContract())
+    kommisar.kill(await mafia.address, await mafia.typeContract());
+    assert.equal(await mafia.isDead(), true);
+    assert.equal(await citizen2.isDead(), true);
+    assert.equal(await citizen1.isDead(), true);
+  });
+  it("Doctor can heal", async function() {
+    
   });
 });
