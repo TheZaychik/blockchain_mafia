c5fb999db51aa9c7b7acf11a6c1dbae48a5fbba4
Nikita Zaytsev <nikitos945max@gmail.com>
2018-07-16 10:26:20 +0300

    citizen upd

--- contracts/citizen.sol
+++ contracts/citizen.sol
@@ -4,7 +4,7 @@ contract Citizen {
   bool private isDead;
   uint private votes;
   bool private isSleeping;
-  uint public constant tip = 0; // citizen = 0, doctor = 1, donmafia = 2, kommisar = 3, mafia = 4, maniac = 5
+  uint public constant typeContract = 0; // citizen = 0, doctor = 1, donmafia = 2, kommisar = 3, mafia = 4, maniac = 5
 
   constructor() {
     isDead = false;
@@ -16,17 +16,41 @@ contract Citizen {
     votes += 1;
   }
 
+  function sleep() {
+    isSleeping = true;
+  }
+
+  function stopSleep() {
+    isSleeping = false;
+  }
+
   function getVotes() returns (uint) {
     return votes;
   }
 
-  function Kill() {
+  function kill() {
     isDead = true;
   }
 
-  function Vote(address addr) {
+  function vote(address addr, uint typeContr) {
+    if (typeContr = 0){
 
+    }
+    else if (typeContr = 1){
+
+    }
+    else if (typeContr = 2){
+
+    }
+    else if (typeContr = 3){
+
+    }
+    else if (typeContr = 4){
+
+    }
+    else if (typeContr = 5){
+
+    }
   }
-
 
 }
