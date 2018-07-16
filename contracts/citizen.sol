92ad8d4247f60bc9560bb2057ec945f12971637b
Nikita Zaytsev <nikitos945max@gmail.com>
2018-07-16 11:16:49 +0300

    сitizen upd

--- contracts/citizen.sol
+++ contracts/citizen.sol
@@ -1,11 +1,18 @@
 pragma solidity ^0.4.22;
 
+import "./citizen.sol";
+import "./doctor.sol";
+import "./donmafia.sol";
+import "./kommisar.sol";
+import "./mafia.sol";
+import "./mabiac.sol";
+
 contract Citizen {
   bool private isDead;
   uint private votes;
   bool private isSleeping;
-  uint public constant typeContract = 0; // citizen = 0, doctor = 1, donmafia = 2, kommisar = 3, mafia = 4, maniac = 5
 
+  uint public constant typeContract = 0; // citizen = 0, doctor = 1, donmafia = 2, kommisar = 3, mafia = 4, mania
   constructor() {
     isDead = false;
     isSleeping = false;
@@ -34,10 +41,10 @@ contract Citizen {
 
   function vote(address addr, uint typeContr) {
     if (typeContr == 0){
-
+      Citizen(addr).addVote();
     }
     else if (typeContr == 1){
-
+        
     }
     else if (typeContr == 2){
 
